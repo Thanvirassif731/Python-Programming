c33ff030f868b12a73c7b1922d5b0561e662e737
Thanvir Assif <110765434+Thanvirassif731@users.noreply.github.com>
2023-01-19 17:36:08 +0530

    Update file_handling.py

--- file_handling.py
+++ file_handling.py
@@ -1,17 +1,32 @@
+import re
+
 fname = input("Enter the file name : ")
 
 num_words = 0
 
-# Search the file that we gave through input...
-with open(fname,'r') as file:
-    # Assign the file contents and reads the text in it...
-    data = file.read()
-    print(data)
-    # Lines going to split...
-    lines = data.split()
-    num_words += len(lines)
 
-# Finally, Printing the total numbers of words in the text file...
-print("Number of words:")
-print(num_words)
+with open(fname, 'r') as file:
+    data = file.read()                                                          # Reads the text
+    print(data)
+
+    lines = data.split()
+    num_words += len(lines)                                                     # Number of words
+    print(f"Total number of words : {num_words}")
+
+    tot_numeric = re.findall(r"\d+", data)
+    numeric = len(tot_numeric)                                                  # Number of Numerics
+    print(f"Total number of Numerics : {numeric}")                              # prints total numerics
+
+    not_special_char = sum(not data.isalnum() for c in data)                    # Number of Letters {I have used special characters method and setted 'NOT'
+    print(f"Total number of letters : {not_special_char}")
+
+    special_characters = re.findall("[^a-zA-Z0-9 ]", data)                      # Number of special Characters
+    special_char_count = len(special_characters)
+    print(f"Total number of Special Characters : {special_char_count}")         # prints total Special char
+
+
+
+    print(f"\n\nspecial characters are Filtered : {special_characters}")            # prints the filtered special char
+    print(f"Numerics are filtered : {tot_numeric}")                             # prints the filtered numerics
+
 file.close()
